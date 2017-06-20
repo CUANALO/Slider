ae5f2fd7bb6bbd47f1562e1f6b7d91e2aa4ae1a4
CUANALO <cuanalo_rocha@hotmail.com>
2017-06-20 09:59:49 -0500

    Nuebo

---
+++ Galeria/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Galeria from '../index';
+
+describe('<Galeria />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
